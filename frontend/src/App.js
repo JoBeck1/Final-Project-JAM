3ba0c14729ff2bf423f774e639de4b871eb1ee44
malshaheri <malshaheri@gmail.com>
2022-09-27 16:03:32 +0200

    add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import Home from './components/Home.jsx'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Header />
+    
     </div>
   );
 }
