b5d0668122ca0a97734779f2979e1206a320f88c
JoBeck1 <Beckjochen@yahoo.de>
2022-09-28 15:38:37 +0200

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 
 import Home from './components/Home';
 import RegisterForm from './components/RegisterForm';
+import {Router, Routes} from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
  <Home/>
  <RegisterForm />
+<Routes>
+
+</Routes>
+
+
     </div>
   );
 }
