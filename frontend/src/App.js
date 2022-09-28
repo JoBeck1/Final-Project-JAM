ed0e5a05f0f0718fa69affc4fe75af8d67382580
ahmed-rahama <ahmmmost@outlook.com>
2022-09-28 14:05:32 +0200

    registerform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import Home from './components/Home';
+import RegisterForm from './components/RegisterForm';
 function App() {
   return (
     <div className="App">
  <Home/>
-    
+ <RegisterForm />
     </div>
   );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Main from './Main.jsx'
 import Header from "./Header.jsx"
 
+
 function Home() {
   return (
     <div>
          <Header />
        
         <Main />
+      
 
     </div>
 
