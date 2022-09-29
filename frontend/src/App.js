94e6627f4080ae2e0a9fc039f1562f0738ea89e3
JoBeck1 <Beckjochen@yahoo.de>
2022-09-29 16:22:29 +0200

    Added Flashcard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 
 import Home from './components/Home';
 import RegisterForm from './components/RegisterForm';
+import FlashcardCreate from './components/FlashcardCreate';
 import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
  <Routes>
   <Route path= "/" element= {<Home/>}/>
   <Route path="/signup" element= {<RegisterForm />}/>
+  <Route path="/flashcardcreateNL" element= {<FlashcardCreate />}/>
   <Route path= "/login" />
  </Routes>
     </div>
