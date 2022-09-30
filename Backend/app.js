01f5f07304ee95229a9af2b9ddb08227dca2d2bb
ahmed-rahama <ahmmmost@outlook.com>
2022-09-30 15:52:51 +0200

    update usercontroller

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express'); 
 const app = express(); 
+const cors = require('cors');
 app.use(express.json());
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+
+app.use(cors())
 const port = process.env.Port || 5000; 
 const testRouter = require("./Routes/Test.js")
 
@@ -14,7 +17,17 @@ mongoose.connect("mongodb://localhost:27017/projecta?readPreference=primary&appn
 
 app.use("/test", testRouter);
 
-app.use("/signup", UserRouter)
+app.use("/signup", UserRouter);
+
+//Global error handler
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error :{
+            msg:error.message
+        }
+    })
+})
 
 app.listen(port, ()=>{console.log(`Listening to server on port ${port}.`)})
 
