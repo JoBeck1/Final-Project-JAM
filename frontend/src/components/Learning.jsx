b07f69334d82949b6f28506c32cafd8596150312
malshaheri <malshaheri@gmail.com>
2022-10-25 05:11:15 +0200

    some styling_24-10

--- frontend/src/components/Learning.jsx
+++ frontend/src/components/Learning.jsx
@@ -77,7 +77,6 @@ const FlashcardContainer = styled.div`
   height: 65vh;
   justify-content: center;
   align-items: center;
-  background-color: #8b8888; 
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 10px;
--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Context } from '../store/Store';
-import { motion, useAnimation } from 'framer-motion';
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/Store";
+import { motion, useAnimation } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import '../styles/userProfile.css';
+import "../styles/userProfile.css";
 
 // const userData = await axios.get("/user/profile")
 // const user = userData.user
@@ -12,22 +12,22 @@ import '../styles/userProfile.css';
 function UserProfile() {
   let [user, setUser] = useState();
   useEffect(async () => {
-    const userData = await axios.get('/profile');
+    const userData = await axios.get("/profile");
     //user = userData.data.user;
     setUser(userData.data.user);
     console.log(user);
   }, []);
   // const {user}= useContext(Context)
-  let text="click on Your name to see your information⬇️"
+  let text = "click on Your name to see your information⬇️";
 
-  let letters= Array.from(text)
-  const clickHierContainer= {
+  let letters = Array.from(text);
+  const clickHierContainer = {
     hidden: { opacity: 0 },
     visible: (i = 1) => ({
       opacity: 1,
       transition: { staggerChildren: 0.04, delayChildren: 0.04 * i },
     }),
-  }
+  };
   const child = {
     visible: {
       opacity: 1,
@@ -50,7 +50,6 @@ function UserProfile() {
       },
     },
   };
-  
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -65,10 +64,21 @@ function UserProfile() {
     <div className="main-profile-container">
       {user ? (
         <div className="userContainer">
-            <motion.div className="click-hier" variants={clickHierContainer} initial="hidden" animate="visible" style={{overflow: "hidden", fontSize:"1.8rem"}}> {letters.map((letter, index) => 
-               (<motion.span style={{color:"#48aeda"}}
-              variants={child} >       {letter === " " ? "\u00A0" : letter} </motion.span>)
-            )}  </motion.div>
+          <motion.div
+            className="click-hier"
+            variants={clickHierContainer}
+            initial="hidden"
+            animate="visible"
+            style={{ overflow: "hidden", fontSize: "1.8rem" }}
+          >
+            {" "}
+            {letters.map((letter, index) => (
+              <motion.span style={{ color: "#48aeda" }} variants={child}>
+                {" "}
+                {letter === " " ? "\u00A0" : letter}{" "}
+              </motion.span>
+            ))}{" "}
+          </motion.div>
           <div className="box-container">
             <motion.div
               className="box"
@@ -76,18 +86,18 @@ function UserProfile() {
               transition={{
                 layout: {
                   duration: 1,
-                  type: 'spring',
+                  type: "spring",
                 },
               }}
               layout
               style={{
-                borderRadius: '1rem',
-                boxShadow: '0px 10px 30px rgba(0,0,0,0.5',
+                borderRadius: "1rem",
+                boxShadow: "0px 10px 30px rgba(0,0,0,0.5",
               }}
             >
               <motion.h2 layout="position">
-                {' '}
-                Hi {`${user.firstName} ${user.Surname}`}{' '}
+                {" "}
+                Hi {`${user.firstName} ${user.Surname}`}{" "}
               </motion.h2>
               {isOpen && (
                 <motion.div
@@ -96,9 +106,9 @@ function UserProfile() {
                     staggerChildren: 0.4,
                     opacity: 0.8,
                     transition: { duration: 1 },
-                    x: '0',
+                    x: "0",
                   }}
-                  initial={{ x: '-100px', opacity: 0 }}
+                  initial={{ x: "-100px", opacity: 0 }}
                 >
                   <h4> your name : {user.firstName} </h4>
                   <h4> your Surname: {user.Surname}</h4>
@@ -111,44 +121,43 @@ function UserProfile() {
         </div>
       ) : (
         <div className="not-login">
-          {' '}
+          {" "}
           <h2> you are not registered </h2>
         </div>
       )}
 
       <div>
-      <button
-            style={{
-              width: "250px",
-              height: "50px",
-              backgroundColor: "#494d58",
-              color: "white",
-              border: "none",
-              outline: "none",
-              fontSize: "20px",
-              borderRadius: "40px",
-              cursor: "pointer",
-              textAlign: "center",
-              boxShadow: " 0 6px 20px -5px rgba(0,0,0,0.4)",
-              position: "relative",
-              overflow: "hidden",
-              transition: "all 0.3s ease",
-              zIndex: "0",
-              marginTop: "20px",
-              marginBottom: "20px",
-              textDecoration: "none",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontWeight: "bold",
-              letterSpacing: "1px",
-              textTransform: "uppercase",
-
-            }}
-            onClick={handleClick}
-          >
-            Back to Home
-          </button>
+        <button
+          style={{
+            width: "250px",
+            height: "50px",
+            backgroundColor: "#494d58",
+            color: "white",
+            border: "none",
+            outline: "none",
+            fontSize: "20px",
+            borderRadius: "40px",
+            cursor: "pointer",
+            textAlign: "center",
+            boxShadow: " 0 6px 20px -5px rgba(0,0,0,0.4)",
+            position: "relative",
+            overflow: "hidden",
+            transition: "all 0.3s ease",
+            zIndex: "0",
+            marginTop: "20px",
+            marginBottom: "20px",
+            textDecoration: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontWeight: "bold",
+            letterSpacing: "1px",
+            textTransform: "uppercase",
+          }}
+          onClick={handleClick}
+        >
+          Back to Home
+        </button>
       </div>
     </div>
   );
